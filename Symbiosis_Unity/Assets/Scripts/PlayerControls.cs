7d46baeefdb0fa79af7cc2dca1b6b7ed4a590321
Paul Conway <paulconway0@gmail.com>
2014-02-13 16:10:34 +0000

    Unity Project Files and Initial Player Controls

---
+++ Symbiosis_Unity/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public float acceleration;
+	public float turnSpeed;
+	public float currSpeed, maxSpeed;
+	float horiz, vert;
+    Transform myTransform;
+
+	void Start () 
+	{   
+        myTransform = this.transform;
+	}
+
+	void Update () 
+	{
+        Controls();
+	}
+
+    void Controls()
+    {
+        horiz = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+        Move();
+    }
+
+    void Move()
+    {
+        rigidbody.AddForce(myTransform.up * vert * acceleration * Time.deltaTime);
+      //  rigidbody.AddTorque(-horiz * turnSpeed * Time.deltaTime);
+    }
+}
