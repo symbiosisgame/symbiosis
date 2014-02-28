eeaf4ec8ba6d7084ade977fa5a0269debc676549
Claudio Medina <missthetarget@gmail.com>
2014-02-28 02:30:16 +0000

    no message

--- Symbiosis_Unity/Assets/Scripts/Ship Scripts/BelayScript.cs
+++ Symbiosis_Unity/Assets/Scripts/Ship Scripts/BelayScript.cs
@@ -4,73 +4,73 @@ using System.Collections;
 // BelayScript should be attached to the PlayerManager whilst ShipTriggerManager goes onto any triggers
 // Belay handles the logic for docked ships and other entities interacting the trigger and belays an action/order
 
-public class BelayScript : MonoBehaviour 
+public class BelayScript : MonoBehaviour
 {
-	void OnEnable()
-	{
-		ShipTriggerManager.Docking += Docking;		// OnTriggerEnter
-		ShipTriggerManager.IsDocked += Docked;		// OnTriggerStay
-		ShipTriggerManager.HasUnDocked += UnDock;	// OnTriggerExit
-	}
-	
-	
-	void OnDisable()
-	{
-		ShipTriggerManager.Docking -= Docking;
-		ShipTriggerManager.IsDocked -= Docked;
-		ShipTriggerManager.HasUnDocked += UnDock;
-	}
+    void OnEnable()
+    {
+        ShipTriggerManager.Docking += Docking;		// OnTriggerEnter
+        ShipTriggerManager.IsDocked += Docked;		// OnTriggerStay
+        ShipTriggerManager.HasUnDocked += UnDock;	// OnTriggerExit
+    }
 
 
-	// events
+    void OnDisable()
+    {
+        ShipTriggerManager.Docking -= Docking;
+        ShipTriggerManager.IsDocked -= Docked;
+        ShipTriggerManager.HasUnDocked += UnDock;
+    }
 
 
-	void Docking(Collider other)
-	{
-		Debug.Log( "Docking..." + other.name );
-<<<<<<< HEAD
-	}
-	
-=======
-	}	
-
->>>>>>> cbbafcb8229cc21bbcd6707a3775ba9ffdfe498b
-	void Docked(Collider other)
-	{
-		if(other.name == "Player1")
-		{
-			Debug.Log( "This Player Docked: " + other.name );
-			Feeder.feederDocked = true;
-		}
-		if(other.name == "Player2")
-		{
-			Debug.Log( "This Player Docked: " + other.name );
-			Protector.protectorDocked = true;
-		}
-	}
-	
+    // events
 
 
-	void UnDock(Collider other)
-	{
-		if(other.name == "Player1")
-		{
-			Debug.Log( "UnDock'ed..." + other.name );
-			Feeder.feederDocked = false;
-		}
-		if(other.name == "Player2")
-		{	
-			Debug.Log( "UnDock'ed..." + other.name );
-			Protector.protectorDocked = false;
-		}
-	}
+    void Docking(Collider other)
+    {
+        Debug.Log("Docking..." + other.name);
+    }
+
+
+
+    void Docked(Collider other)
+    {
+        if (other.name == "Player1")
+        {
+            Debug.Log("This Player Docked: " + other.name);
+            Feeder.feederDocked = true;
+        }
+        if (other.name == "Player2")
+        {
+            Debug.Log("This Player Docked: " + other.name);
+            Protector.protectorDocked = true;
+        }
+    }
+
+
+
+    void UnDock(Collider other)
+    {
+        if (other.name == "Player1")
+        {
+            Debug.Log("UnDock'ed..." + other.name);
+            Feeder.feederDocked = false;
+        }
+        if (other.name == "Player2")
+        {
+            Debug.Log("UnDock'ed..." + other.name);
+            Protector.protectorDocked = false;
+        }
+    }
 
 
 
 
-}// end class
-//------------------------------------------------------
+    // end class
+    //------------------------------------------------------
 
-//-------- notes
-// scenario:  if player docked and other enemy docking then set docked player shields up
-// scenario:  if player docked and other enemy docking then set undocked player beacon alert
+    //-------- notes
+    // scenario:  if player docked and other enemy docking then set docked player shields up
+    // scenario:  if player docked and other enemy docking then set undocked player beacon alert
+
+
+}
