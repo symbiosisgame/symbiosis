9ec471e5beff660d01d22e32fb3804fadf0ba634
Paul Conway <paulconway0@gmail.com>
2014-04-26 16:37:24 +0100

    Controls Updated

--- Symbiosis_Unity/Assets/Scripts/Player Scripts/PlayerControls.cs
+++ Symbiosis_Unity/Assets/Scripts/Player Scripts/PlayerControls.cs
@@ -8,8 +8,8 @@ public class PlayerControls : PlayerManager {
 	public float decceleration = 0.95f, despin = 0.93f; //for slowing down move and turn speed
 
 	private float currSpeedP1, currSpeedP2; //not used yet, just stores current speeds
-	private float horiz1, vert1, horiz2, vert2, horiz3, vert3, horiz4, vert4, reverse1, reverse2; //stores value of axis, less or greater than 0 determines positive/negative direction
-	bool triggerControls = true;
+	private float forwardP1, forwardP2, reverseP1, reverseP2, rotateP1, rotateP2, moveKeyboardP1, moveKeyboardP2, rotateKeyboardP1, rotateKeyboardP2, moveJoyP1, moveJoyP2, rotateJoyP1, rotateJoyP2; //stores value of axis, less or greater than 0 determines positive/negative direction
+	bool alternateControls = true;
 
 	new void Start () 
 	{   
@@ -25,144 +25,173 @@ public class PlayerControls : PlayerManager {
 		feederAnim["FeederTurnRight"].speed = 0.5f;
 	}
 
-	void FixedUpdate () //better for physics calculations
+	void Update()
 	{
 		StoreAxis();
 		CalculateDistance();
-		MovePlayer1();
-		MovePlayer2();
 		Player1Ability();
 		Player2Ability();
 		InputScheme();
+	}
+
+	void FixedUpdate () //better for physics calculations
+	{
+		MovePlayer1();
+		MovePlayer2();
 	}
 
 	void InputScheme()
 	{
 		if(Input.GetButtonDown("Select"))
 		{
-			triggerControls = !triggerControls;
+			alternateControls = !alternateControls;
 		}
 	}
 
 	void StoreAxis()
 	{
-		if(triggerControls)
+		if(alternateControls)
 		{
-			//joypad controls
+			//joypad controls, button to move
 			//player 1
-			vert1 = Input.GetAxis ("VerticalP1Trigger"); //right trigger move forward
-			reverse1 = Input.GetAxis("ReverseP1Trigger"); //left trigger move backward
-			horiz1 = Input.GetAxis("HorizontalP1Trigger"); //left stick for turning
-
+			forwardP1 = Input.GetAxis ("ForwardTriggerP1"); //right trigger move forward
+			reverseP1 = Input.GetAxis("ReverseTriggerP1"); //left trigger move backward
+			rotateP1 = Input.GetAxis("RotateStickP1"); //left stick for turning
 			//player 2
-			vert2 = Input.GetAxis ("VerticalP2Trigger"); //right trigger move forward
-			reverse2 = Input.GetAxis("ReverseP2Trigger"); //left trigger move backward
-			horiz2 = Input.GetAxis("HorizontalP2Trigger"); //left stick for turning
-
-			//keyboard controls
-			horiz3 = Input.GetAxis("HorizontalP1"); //P1 A & D rotate left & right
-			vert3 = Input.GetAxis("VerticalP1"); //P1 W & S move forward & back
-			horiz4 = Input.GetAxis("HorizontalP2"); //P2 left & right rotate left & right
-			vert4 = Input.GetAxis("VerticalP2"); //P2 up & down move forward & back
-			//maxSpeed = 6f;*/
+			forwardP2 = Input.GetAxis ("ForwardTriggerP2"); //right trigger move forward
+			reverseP2 = Input.GetAxis("ReverseTriggerP2"); //left trigger move backward
+			rotateP2 = Input.GetAxis("RotateStickP2"); //left stick for turning
 		}
 		else
 		{
-			//joypad controls
-			vert1 = Input.GetAxis("VerticalP1Joy");
-			horiz1 = Input.GetAxis("HorizontalP1Joy"); 
-			vert2 = Input.GetAxis("VerticalP2Joy"); //right stick rotate
-			horiz2 = Input.GetAxis("HorizontalP2Joy"); 
-
-			//keyboard controls
-			vert3 = Input.GetAxis("VerticalP1");
-			horiz3 = Input.GetAxis("HorizontalP1"); 
-			vert4 = Input.GetAxis("VerticalP2");
-			horiz4 = Input.GetAxis("HorizontalP2");
+			//alternate joypad controls, tank
+			//player 1
+			forwardP1 = Input.GetAxis("VerticalP1Joy");
+			rotateP1 = Input.GetAxis("HorizontalP1Joy"); 
+			//player 2
+			forwardP2 = Input.GetAxis("VerticalP2Joy"); //right stick rotate
+			rotateP2 = Input.GetAxis("HorizontalP2Joy"); 
 		}
+		moveKeyboardP1 = Input.GetAxis("MoveKeyboardP1"); //P1 W & S move forward & back
+		rotateKeyboardP1 = Input.GetAxis("RotateKeyboardP1"); //P1 A & D rotate left & right
+		moveKeyboardP2 = Input.GetAxis("MoveKeyboardP2"); //P2 up & down move forward & back
+		rotateKeyboardP2 = Input.GetAxis("RotateKeyboardP2"); //P2 left & right rotate left & right
 	}
 	
 	void MovePlayer1()
     {
-		if(triggerControls) //this set of controls is push trigger to move and keyboard controls
+		if(alternateControls) //this set of controls is push trigger to move and keyboard controls
 		{
-			player1.rigidbody.AddForce(p1Transform.up * vert1 * acceleration * Time.deltaTime); //joypad
-			player1.rigidbody.AddForce(-p1Transform.up * reverse1 * acceleration * Time.deltaTime); //joypad
-
-			//player1.rigidbody.AddForce(p1Transform.up * vert3 * acceleration * Time.deltaTime); //keyboard
+			if(player1.rigidbody.velocity.magnitude < maxSpeed)
+			{
+				if(Input.GetButton("ForwardTriggerP1"))
+				{
+					player1.rigidbody.AddForce(p1Transform.up * acceleration * Time.deltaTime); 
+				}
+				if(Input.GetButton("ReverseTriggerP1"))
+				{
+					player1.rigidbody.AddForce(-p1Transform.up * acceleration * Time.deltaTime); 
+				}
+			}
+			if(player1.rigidbody.angularVelocity.magnitude < maxTurnSpeed) //this is to handle the rotation, both joypad and keyboard
+			{
+				player1.rigidbody.AddRelativeTorque(-p1Transform.forward * rotateP1 * turnAcceleration * Time.deltaTime); //joypad
+			}
 		}
 		else //this set of controls is the movement mapped to the analog sticks on control and keyboard controls
 		{
 			if(player1.rigidbody.velocity.magnitude < maxSpeed)
 			{
-				player1.rigidbody.AddForce(p1Transform.up * vert1 * acceleration * Time.deltaTime); //joypad
-				//player1.rigidbody.AddForce(p1Transform.up * vert3 * acceleration * Time.deltaTime); //keyboard
+				player1.rigidbody.AddForce(p1Transform.up * forwardP1 * acceleration * Time.deltaTime); //joypad
+			}
+			if(player1.rigidbody.angularVelocity.magnitude < maxTurnSpeed) //this is to handle the rotation, both joypad and keyboard
+			{
+				player1.rigidbody.AddRelativeTorque(-p1Transform.forward * rotateP1 * turnAcceleration * Time.deltaTime); //joypad
 			}
 		}
-
+		//Keyboard controls
+		if(player1.rigidbody.velocity.magnitude < maxSpeed)
+		{
+			player1.rigidbody.AddForce(p1Transform.up * moveKeyboardP1 * acceleration * Time.deltaTime); //keyboard movement
+		}
 		if(player1.rigidbody.angularVelocity.magnitude < maxTurnSpeed) //this is to handle the rotation, both joypad and keyboard
 		{
-			player1.rigidbody.AddRelativeTorque(-p1Transform.forward * horiz1 * turnAcceleration * Time.deltaTime); //joypad
-			//player1.rigidbody.AddRelativeTorque(-p1Transform.forward * horiz3 * turnAcceleration * Time.deltaTime); //keyboard
+			player1.rigidbody.AddRelativeTorque(-p1Transform.forward * rotateKeyboardP1 * turnAcceleration * Time.deltaTime); //keyboard movement
 		}
-
-		/*currSpeedP1 = player1.rigidbody.velocity.magnitude;
+		currSpeedP1 = player1.rigidbody.velocity.magnitude;
 		if(currSpeedP1 <= 0.01f)
 		{
 			currSpeedP1 = 0;
 			feederAnim.CrossFade("FeederIdle", .2f);
 		}
 		
-		if(horiz1 < 0)
+		if(rotateP1 < 0)
 		{
 			//feederAnim.CrossFade("FeederTurnLeft", .2f); 
 		}
-		else if(horiz1 > 0)
+		else if(rotateP1 > 0)
 		{
 			//feederAnim.CrossFade("FeederTurnRight", .2f);
 		}
 		else
 		{
 			feederAnim.CrossFade("FeederIdle", .2f);
-		}*/
+		}
     }
 
 	void MovePlayer2()
 	{
-		if(triggerControls) //this set of controls is push trigger to move and keyboard controls
+		if(alternateControls) //this set of controls is push trigger to move and keyboard controls
 		{
-			player2.rigidbody.AddForce(p2Transform.up * vert2 * acceleration * Time.deltaTime);//joypad
-			player2.rigidbody.AddForce(-p2Transform.up * reverse2 * acceleration * Time.deltaTime); //joypad
-		//	player2.rigidbody.AddForce(p2Transform.up * vert4 * acceleration * Time.deltaTime);//keyboard
+			if(player2.rigidbody.velocity.magnitude < maxSpeed)
+			{
+				if(Input.GetButton("ForwardTriggerP2"))
+				{
+					player2.rigidbody.AddForce(p2Transform.up * acceleration * Time.deltaTime); //joypad
+				}
+				if(Input.GetButton("ReverseTriggerP2"))
+				{
+					player2.rigidbody.AddForce(-p2Transform.up * acceleration * Time.deltaTime); //joypad
+				}
+			}
+			if(player2.rigidbody.angularVelocity.magnitude < maxTurnSpeed) //this is to handle the rotation, both joypad and keyboard
+			{
+				player2.rigidbody.AddRelativeTorque(-p2Transform.forward * rotateP2 * turnAcceleration * Time.deltaTime); //joypad
+			}
 		}
 		else //this set of controls is the movement mapped to the analog sticks on control and keyboard controls
 		{
 			if(player2.rigidbody.velocity.magnitude < maxSpeed)
 			{
-				player2.rigidbody.AddForce(p2Transform.up * vert2 * acceleration * Time.deltaTime);//joypad
-				//player2.rigidbody.AddForce(p2Transform.up * vert4 * acceleration * Time.deltaTime);//keyboard
+				player2.rigidbody.AddForce(p2Transform.up * forwardP2 * acceleration * Time.deltaTime); //joypad
+			}
+			if(player2.rigidbody.angularVelocity.magnitude < maxTurnSpeed) //this is to handle the rotation, both joypad and keyboard
+			{
+				player2.rigidbody.AddRelativeTorque(-p2Transform.forward * rotateP2 * turnAcceleration * Time.deltaTime); //joypad
 			}
 		}
-
-
-		if(player2.rigidbody.angularVelocity.magnitude < maxTurnSpeed) //rotation player 2
+		//Keyboard controls
+		if(player2.rigidbody.velocity.magnitude < maxSpeed)
 		{
-			player2.rigidbody.AddRelativeTorque(-p2Transform.forward * horiz2 * turnAcceleration * Time.deltaTime);//joypad
-			//player2.rigidbody.AddRelativeTorque(-p2Transform.forward * horiz4 * turnAcceleration * Time.deltaTime);//keyboard
+			player2.rigidbody.AddForce(p2Transform.up * moveKeyboardP2 * acceleration * Time.deltaTime); //keyboard movement
 		}
+		if(player2.rigidbody.angularVelocity.magnitude < maxTurnSpeed) //this is to handle the rotation, both joypad and keyboard
+		{
+			player2.rigidbody.AddRelativeTorque(-p2Transform.forward * rotateKeyboardP2 * turnAcceleration * Time.deltaTime); //keyboard movement
+		}
+
 		
-		/*currSpeedP2 = player2.rigidbody.velocity.magnitude;
+		currSpeedP2 = player2.rigidbody.velocity.magnitude;
 		if(currSpeedP2 <= 0.01f)
 		{
-			currSpeedP2 = 0;
-
+			currSpeedP2 = 0f;
 		}
 
-		if(vert2 < 0)
+		if(forwardP2 < 0)
 		{
 			protAnim.CrossFade("ProtMove", .6f);
 		}
-		else if(vert2 > 0)
+		else if(forwardP2 > 0)
 		{
 			protAnim.CrossFade("ProtMove", .6f);
 		}
@@ -170,14 +199,14 @@ public class PlayerControls : PlayerManager {
 		{
 			protAnim.CrossFade("ProtIdle", .4f); 
 		}
-		if(horiz2 < 0)
+		if(rotateP2 < 0)
 		{
 			//protAnim.CrossFade("ProtTurnRight", .4f); 
 		}
-		if(horiz2 > 0)
+		if(rotateP2 > 0)
 		{
 			//protAnim.CrossFade("ProtTurnLeft", .4f);
-		}*/
+		}
 	}
 
 	void Player1Ability()
@@ -193,12 +222,12 @@ public class PlayerControls : PlayerManager {
 	
 	void Player2Ability()
 	{
-		if(Input.GetButtonDown ("Taunt")) //add xbox button
+		if(Input.GetButtonDown ("Taunt")) 
 		{
 			if(protector.currentFood > 0)
 			{
-				protAnim.CrossFade("ProtTaunt", .4f);
-				protector.Taunt(protectorGO.transform.position, 2f, -1);
+				protAnim.Play("ProtTaunt");
+				protector.Taunt(protectorGO.transform.position, 2.5f, -1);
 			}
 		}
 	}
@@ -224,5 +253,4 @@ public class PlayerControls : PlayerManager {
 	{
 		return distanceTo;
 	}
-
 }
